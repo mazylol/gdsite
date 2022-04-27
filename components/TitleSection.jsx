bc34d37b8da9c03a91fc495576ded5b5067ffd86
mazylol <mail@mazy.tk>
2022-04-27 18:56:50 -0400

    center the text

--- components/TitleSection.jsx
+++ components/TitleSection.jsx
@@ -2,7 +2,7 @@ export default function TitleSection() {
   return (
     <div className="flex w-full h-80 bg-gradient-to-r from-yellow-400 via-green-500 to-blue-500">
       <div className="mx-auto my-auto">
-        <p className="text-white text-7xl font-extrabold">Game Design</p>
+        <p className="text-center text-white text-7xl font-extrabold">Game Design</p>
         <p className="text-center text-white text-xl font-light pt-2">
           We make games
         </p>

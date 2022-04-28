f5f782124990c32c0a708f10c27447d20ea480b1
mazylol <mail@mazy.tk>
2022-04-27 22:23:40 -0400

    new navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,88 +3,27 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import { BsController } from 'react-icons/bs';
 import Link from 'next/link';
 
-const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Showcase', href: '#', current: false },
-  { name: 'Files', href: '#', current: false },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-zinc-900">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <Link href="#">
-                    <a>
-                      <div className="block h-8 w-auto">
-                        <BsController className="text-4xl text-gray-400" />
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-zinc-600 text-white'
-                            : 'text-gray-300 hover:bg-zinc-600 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-zinc-600 text-white'
-                      : 'text-gray-300 hover:bg-zinc-600 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  );
+    <nav className="flex items-center justify-between flex-wrap bg-zinc-900 p-3">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <a href="#">
+          <BsController className="text-gray-300 flex text-5xl" />
+        </a>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 text-md">
+            Files
+          </a>
+          <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 text-md">
+            Showcase
+          </a>
+          <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white text-md">
+            Games
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
 }
--- components/TitleSection.jsx
+++ components/TitleSection.jsx
@@ -1,6 +1,6 @@
 export default function TitleSection() {
   return (
-    <div className="flex w-full h-80 bg-gradient-to-r from-yellow-400 via-green-500 to-blue-500">
+    <div className="flex w-full h-80 bg-gradient-to-r from-yellow-400 via-green-500 to-blue-700">
       <div className="mx-auto my-auto">
         <p className="text-center text-white text-7xl font-extrabold">Game Design</p>
         <p className="text-center text-white text-xl font-light pt-2">

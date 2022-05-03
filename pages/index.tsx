e348f5f57e502b6e5632904fe497f34afc6d5a5e
mazylol <mail@mazy.tk>
2022-05-02 21:18:04 -0400

    list

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,19 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <>
-    <div className="min-h-screen bg-zinc-900">
-      <div className="flex w-full h-64 bg-gradient-to-r from-yellow-400 via-green-500 to-blue-700">
-        <div className="mx-auto my-auto">
-          <p className="text-center text-white text-7xl font-extrabold">
-            Game Design
-          </p>
-          <p className="text-center text-white text-xl font-light pt-2">
-            We make games
-          </p>
+      <div className="min-h-screen bg-zinc-900">
+        <div className="flex w-full h-64 bg-gradient-to-r from-yellow-400 via-green-500 to-blue-700">
+          <div className="mx-auto my-auto">
+            <p className="text-center text-white text-7xl font-extrabold">
+              Game Design
+            </p>
+            <p className="text-center text-white text-xl font-light pt-2">
+              We make games
+            </p>
+          </div>
         </div>
+        <Info />
       </div>
-      <Info />
-    </div>
     </>
   );
 }
